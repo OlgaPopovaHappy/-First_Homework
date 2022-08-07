14d59ba342ffc10ac22ef1bb74d13b0c896b0d7d
OlgaPopovaHappy <109219990+OlgaPopovaHappy@users.noreply.github.com>
2022-08-07 10:52:11 +0300

    С# ДЗ_1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console; 
+Clear();
+//Задача 1 
+//   **Задача 4:** Напишите программу, которая принимает на вход три числа 
+//  и выдаёт максимальное из этих чисел.
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+
+Clear();
+
+WriteLine("Введите три числа: ");
+
+int a= Convert.ToInt32(ReadLine());
+int b =Convert.ToInt32(ReadLine());
+int c =Convert.ToInt32(ReadLine());
+int max = a;
+if (b>max)
+{
+     max = b;
+} 
+if (c>max)
+{
+     max = c;
+} 
+WriteLine(max);
